5210e62e9f9f00c6d12a896f08623835d966de3a
Matt Glaman <nmd.matt@gmail.com>
2018-08-29 21:03:39 -0500

    Add civicrm_contact element

---
+++ src/Element/CivicrmContact.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\webform_civicrm\Element;
+
+use Drupal\Core\Render\Element\FormElement;
+use Drupal\Core\Render\Element\Textfield;
+
+/**
+ * @FormElement("civicrm_contact")
+ */
+class CivicrmContact extends Textfield {
+
+}
